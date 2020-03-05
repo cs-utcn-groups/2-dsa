80706bbf5d2769b7b0c8382642fe59576320bfa0
Borbala Fazakas <fazakasbori@gmail.com>
2020-03-06 00:30:19 +0200

    1st commit: queue implemented, data of timings read from file

--- 30412/Fazakas Borbala(Bori)/assignment2_queueAuchan/main.c
+++ 30412/Fazakas Borbala(Bori)/assignment2_queueAuchan/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "list.h"
 
 #define FILE_ERR_CODE 1
 #define FILE_ERR_MESS "error opening file"
+#define MAX_LINE_LENGTH 1000
 
 FILE *openFile(char *name, char *mode) {
     FILE *newFile = fopen(name, mode);
@@ -18,5 +20,19 @@ int main() {
     FILE *inFile = openFile("input.dat", "r");
     FILE *outFile = openFile("output.dat", "w");
 
+    Queue moments = createQueue();
+
+
+    char *line = (char *) malloc(sizeof(char) * MAX_LINE_LENGTH);
+    fgets(line, MAX_LINE_LENGTH, inFile);
+    const char s[1] = " ";
+    char* nextMoment_string;
+
+    nextMoment_string = strtok(line, s);
+    push(&moments, atoi(nextMoment_string));
+    while ((nextMoment_string = strtok(NULL, s)) != NULL) {
+        push(&moments, atoi(nextMoment_string));
+        printAll(&moments, stdout);
+    }
     return 0;
 }
