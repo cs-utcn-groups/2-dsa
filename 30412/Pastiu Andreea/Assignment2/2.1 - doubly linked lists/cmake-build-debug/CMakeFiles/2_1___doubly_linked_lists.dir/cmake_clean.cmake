42e04d353691c6fe7f35e407f8a6d756ab363b32
Andreea Pastiu <andreea_pastiu99@yahoo.com>
2020-03-07 19:04:01 +0200

    initial commit

---
+++ 30412/Pastiu Andreea/Assignment2/2.1 - doubly linked lists/cmake-build-debug/CMakeFiles/2_1___doubly_linked_lists.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2_1___doubly_linked_lists.exe"
+  "2_1___doubly_linked_lists.exe.manifest"
+  "2_1___doubly_linked_lists.pdb"
+  "CMakeFiles/2_1___doubly_linked_lists.dir/main.c.obj"
+  "lib2_1___doubly_linked_lists.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2_1___doubly_linked_lists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 30412/Pastiu Andreea/Assignment2/2.1 - doubly linked lists/cmake-build-debug/CMakeFiles/2_1___doubly_linked_lists.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 2_1___doubly_linked_lists.
+# This may be replaced when dependencies are built.
