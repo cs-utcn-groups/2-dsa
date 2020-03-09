bae860c44ffe5a4c29a782a68a34a35aefedf28d
Diana-Teodora Vrabie <dianateodorav@gmail.com>
2020-03-09 14:42:17 +0200

    add first, print last done

---
+++ 30412/Vrabie Diana Teodora/Assignment 2-1/list.c
@@ -0,0 +1,44 @@
+//
+// Created by diana on 07/03/2020.
+//
+
+#include "list.h"
+
+void initializeList() {
+    myList = (List*) malloc(sizeof(List));
+    myList->nrOfMembers = 0;
+    myList->first = NULL;
+    myList->last = NULL;
+}
+
+void addElementToFront(int data) {
+    if (myList->first == NULL) {
+        myList->first = initializeEmptyNode();
+        myList->last = initializeEmptyNode();
+        myList->first->value = data;
+        myList->first->next = NULL;
+        myList->first->prev = NULL;
+        myList->last = myList->first;
+        myList->nrOfMembers = 1;
+    } else {
+        Node *newNode = initializeEmptyNode();
+        newNode->value = data;
+        newNode->next = myList->first;
+        newNode->prev = NULL;
+        myList->first->prev = newNode;
+        myList->first = newNode;
+        myList->nrOfMembers ++;
+    }
+}
+
+void printAll(FILE *file) {
+    if (myList->nrOfMembers == 0) {
+        return;
+    }
+    Node *node = myList->first;
+    while (node!= NULL) {
+        fprintf(file, "%d ", node->value);
+        node = node->next;
+    }
+    fprintf(file, "\n");
+}
---
+++ 30412/Vrabie Diana Teodora/Assignment 2-1/node.c
@@ -0,0 +1,10 @@
+//
+// Created by diana on 07/03/2020.
+//
+
+#include "node.h"
+
+Node* initializeEmptyNode() {
+    Node* node = (Node*) malloc(sizeof(Node));
+    return node;
+}
