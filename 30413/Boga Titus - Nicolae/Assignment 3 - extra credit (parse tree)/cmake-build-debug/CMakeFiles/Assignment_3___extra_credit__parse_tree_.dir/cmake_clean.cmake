e4ba2a8dc95627a65449a541467c4ec8d935865c
Titus <titusboga@gmail.com>
2020-03-15 01:26:21 +0200

    Message

---
+++ 30413/Boga Titus - Nicolae/Assignment 3 - extra credit (parse tree)/cmake-build-debug/CMakeFiles/Assignment_3___extra_credit__parse_tree_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment_3___extra_credit__parse_tree_.exe"
+  "Assignment_3___extra_credit__parse_tree_.exe.manifest"
+  "Assignment_3___extra_credit__parse_tree_.pdb"
+  "CMakeFiles/Assignment_3___extra_credit__parse_tree_.dir/main.c.obj"
+  "libAssignment_3___extra_credit__parse_tree_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment_3___extra_credit__parse_tree_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
