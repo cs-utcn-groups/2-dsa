14c8547cf3e13236829c5e0de94e3eb533ec8ce8
Andreea Pastiu <andreea_pastiu99@yahoo.com>
2020-03-21 18:14:53 +0200

    initial commit

--- 30412/Pastiu Andreea/Assignment 3/Tree to list and back again/main.c
+++ 30412/Pastiu Andreea/Assignment 3/Tree to list and back again/main.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct vertex
+{
+    int id;
+    struct vertex *left, *right;
+}vertexT;
+
+typedef struct node
+{
+    int id;
+    struct node *next;
+}nodeT;
+
+void prettyPrint(vertexT *pVertex, int i);
+vertexT *getTreeFromList(nodeT *t);
+void traverseList(nodeT *t);
+nodeT *getListFromTree(vertexT *pVertex);
+vertexT *createBinTree();
 
 int main()
 {
-    printf("Hello, World!\n");
+    vertexT *root = createBinTree();
+    nodeT * firstFromList = getListFromTree(root);
+    traverseList(firstFromList);
+    root = getTreeFromList(firstFromList);
+    prettyPrint(root, 0);
+
     return 0;
 }
+
+vertexT *createBinTree()
+{
+    return NULL;
+}
+
+nodeT *getListFromTree(vertexT *pVertex)
+{
+    return NULL;
+}
+
+void traverseList(nodeT *t)
+{
+
+}
+
+vertexT *getTreeFromList(nodeT *t)
+{
+    return NULL;
+}
+
+void prettyPrint(vertexT *pVertex, int i)
+{
+
+}
+
