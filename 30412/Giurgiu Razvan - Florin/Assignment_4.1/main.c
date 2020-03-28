e44d1ddf3ccdd97de27b14c92f68a35a3cfd90b0
giurgiur99 <giurgiur99@gmail.com>
2020-03-28 11:16:21 +0200

    Done..

---
+++ 30412/Giurgiu Razvan - Florin/Assignment_4.1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "avl.h"
+
+void preOrder(NodeT *root)
+{
+    if(root != NULL)
+    {
+        printf("%d ", root->key);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+NodeT *root = NULL;
+
+int main() {
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+
+    preOrder(root);
+    return 0;
+}
