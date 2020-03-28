2dd8b7f55262128412025736c0cafabb696d343f
denisafilip <denifilip02@yahoo.com>
2020-03-28 16:44:18 +0200

    assignment 4 - mandatory & extra

--- 30412/Filip Denisa - Mariana/assignment3Extra/main.c
+++ 30412/Filip Denisa - Mariana/assignment3Extra/main.c
@@ -3,15 +3,21 @@
 #define INPUT_FILE_PATH "../input.dat"
 #define ERROR_MESSAGE "Sorry, the file could not be found"
 
+FILE *openFile (char *name, char *mode);
+
 int main() {
-    FILE *input = fopen(INPUT_FILE_PATH, "r");
-    if (input == NULL) {
-        perror(ERROR_MESSAGE);
-        exit(EXIT_FAILURE);
-    }
+    FILE *input = openFile(INPUT_FILE_PATH, "r");
     Queue myQueue = parseMathematicalExpression(input, &myQueue);
     NodeT *root = createBinTree(&myQueue);
     printTree(root, 0);
     fclose(input);
     return 0;
+}
+
+FILE *openFile (char *name, char *mode) {
+    FILE *newFile = fopen(name, mode);
+    if (newFile == NULL) {
+       perror(ERROR_MESSAGE);
+       exit(EXIT_FAILURE);
+    }
 }
