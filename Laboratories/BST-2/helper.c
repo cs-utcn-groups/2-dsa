cd7dba3e4ffd1c40df0a57cb99e2e78de8c1bd93
Alex <alexx.cosma@gmail.com>
2020-03-30 22:39:18 +0300

    - added BST

---
+++ Laboratories/BST-2/helper.c
@@ -0,0 +1,20 @@
+//
+// Created by Alex on 3/24/2020.
+//
+
+#include "head.h"
+
+void prettyPrint(NodeT *root, int space) {
+    {
+        if (root == NULL) {
+            return;
+        }
+        space += COUNT;
+        prettyPrint(root->right, space);
+        printf("\n");
+        for (int i = COUNT; i < space; i++)
+            printf(" ");
+        printf("%d", root->key);
+        prettyPrint(root->left, space);
+    }
+}
