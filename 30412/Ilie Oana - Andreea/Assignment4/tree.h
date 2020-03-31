3a5e87445e921f8f5e6480597ba8d298eb4b7fec
Andreea Ilie <Andreea.Ilie@student.utcluj.ro>
2020-03-31 11:44:55 +0300

    Assignment 4 + extra - first commit

---
+++ 30412/Ilie Oana - Andreea/Assignment4/tree.h
@@ -0,0 +1,26 @@
+//
+// Created by Andreea on 29-Mar-20.
+//
+
+#ifndef ASSIGNMENT4_TREE_H
+#define ASSIGNMENT4_TREE_H
+
+#include <stdbool.h>
+
+typedef struct _nodeT {
+    int key, height;
+    struct _nodeT *left, *right;
+} nodeT;
+
+nodeT *createNode(int key);
+
+nodeT *insertNode(nodeT *currentRoot, int key);
+
+void prettyPrint(nodeT *currentRoot, int level, FILE *outputFile);
+
+nodeT *deleteNode(nodeT *currentRoot, int key);
+
+
+nodeT *getSuccessor(nodeT *node);
+
+#endif //ASSIGNMENT4_TREE_H
