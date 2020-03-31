7fc2cf4899c6b0a1df1f6646f0d9d903be046367
Adrian-Ioan Bîndilă <bindila.ioan@gmail.com>
2020-03-31 09:08:14 +0300

    Completed assignment 4.

---
+++ 30412/Bindila Adrian Ioan/Assignment 4/tree.h
@@ -0,0 +1,28 @@
+//
+// Created by bindi on 3/26/2020.
+//
+
+#ifndef ASSIGNMENT_4_TREE_H
+#define ASSIGNMENT_4_TREE_H
+typedef struct _tree {
+    double key;
+    int height;
+    struct _tree *left, *right;
+} tree;
+
+tree *createLeaf(double key);
+
+int getHeight(tree *root);
+
+int getBalanceFactor(tree *root);
+
+tree *rotateLeft(tree *root);
+
+tree *rotateRight(tree *root);
+
+tree *balancedInsert(tree *root, double key);
+
+void printTree(tree *root, double level);
+
+
+#endif //ASSIGNMENT_4_TREE_H
