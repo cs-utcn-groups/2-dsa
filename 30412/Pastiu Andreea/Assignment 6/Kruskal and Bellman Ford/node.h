a9a7a8d63b36c4a9b233414c5be283c6ee57e261
Andreea Pastiu <andreea_pastiu99@yahoo.com>
2020-04-13 01:18:25 +0300

    Kruskal & Bellman-Ford

---
+++ 30412/Pastiu Andreea/Assignment 6/Kruskal and Bellman Ford/node.h
@@ -0,0 +1,21 @@
+//
+// Created by Alex on 3/31/2020.
+//
+
+#ifndef GRAPHS___TRAVERSALS_NODE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int content;
+    struct node *next;
+} NodeT;
+
+NodeT *createNode(int content);
+
+#define GRAPHS_101_NODE_H
+
+#define GRAPHS___TRAVERSALS_NODE_H
+
+#endif //GRAPHS___TRAVERSALS_NODE_H
