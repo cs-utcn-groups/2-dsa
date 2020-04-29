3ebbe47b4073e25f01f937a05d0d7e61aecb05f5
Borbala Fazakas <fazakasbori@gmail.com>
2020-04-30 02:11:30 +0300

    table generated

--- 30412/Fazakas Borbala(Bori)/assignment7_HT_chaining/main.c
+++ 30412/Fazakas Borbala(Bori)/assignment7_HT_chaining/main.c
@@ -7,15 +7,15 @@
 
 int main()
 {
-    int noFunctions = 5;
-    int noPossibleSizeFactors = 6;
-    int noPossibleSizes = 8;
+    FILE *tableFile = fopen("table.txt", "w");
+    int noFunctions = 5, noPossibleSizeFactors = 6, noPossibleSizes = 8, functionNr, N;
     int noStrings[8]={100, 500, 100, 5000, 10000, 25000, 50000, 100000};
-    int functionNr, N;
     float sizeFactor;
     double avgSD[MAX_FUNCTIONS + 1] = {0}, SD;
     for(int i=0; i<noPossibleSizes; i++){
         N = noStrings[i];
+        fprintf(tableFile, "-----------------------------------For %d strings--------------------------------------\n",
+                N);
         char** content = readFromFile(N);
         FILE* inputs = fopen("inputs.txt", "r");
         for(int j=1; j<=noFunctions*noPossibleSizeFactors; j++){
@@ -27,14 +27,16 @@ int main()
             }
             SD = standartDeviationOfBucketLengths();
             avgSD[functionNr]+=SD;
-            printf("SD for hash function %d with %d data rows, and initial size factor %f is: %.3lf\n", functionNr, N, sizeFactor, SD);
+            fprintf(tableFile, "HashFunction nr.%d, ISF = %.2f --> standard deviation of buckets sizes = %lf\n",
+                    functionNr, sizeFactor, SD);
         }
         fclose(inputs);
     }
 
+    fprintf(tableFile, "-----------------------------------Average SDs--------------------------------------\n", N);
     for (int i = 0; i < noFunctions; i++) {
         avgSD[i]/=noPossibleSizeFactors*noPossibleSizes;
-        printf("Average standard deviation for hash function nr. %d is %.3lf\n", i, avgSD[i]);
+        fprintf(tableFile, "Average standard deviation for hash function nr. %d is %.3lf\n", i, avgSD[i]);
     }
 
 
