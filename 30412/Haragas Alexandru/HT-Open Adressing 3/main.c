dd9eaeabd83259b6274d5f3e00e0b85a2b19a38a
alexhprime <haragas6@gmail.com>
2020-05-03 14:08:48 +0300

    HA-Open Adressing 3

---
+++ 30412/Haragas Alexandru/HT-Open Adressing 3/main.c
@@ -0,0 +1,29 @@
+#include "io.h"
+#include "files.h"
+#include "hashTable.h"
+
+int main()
+{
+    int N = _100000;
+    char ** content = readFromFile(N);
+    initHashTable(N);
+    int r=0;
+    for(int i=0;i<N;i++)
+    {
+        int c=insertElement(*(content+i));
+        if(c>maxcol)
+            maxcol=c;
+        if(getFillFactor(size)>=MAX_FILL_FACTOR)
+        {
+            r++;
+            resizeHashTable();
+        }
+    }
+    printf("H1: %d %d\n",maxcol,r);
+    //printContentToConsole(content,N);
+    for(int i=0;i<N;i++)
+        free(content[i]);
+    free(content);
+    free(hashTable);
+    return 0;
+}
