6cd7990490039a0b6245cbdb0168cdecd110b914
Anca Itu <itu_anca@yahoo.com>
2020-05-12 15:53:43 +0300

    assignment sorting algorithms

---
+++ 30412/Itu Anca/Assignment_SortingAlgorithms/SortingAlgorithms.h
@@ -0,0 +1,18 @@
+//
+// Created by Anca on 5/10/2020.
+//
+
+#ifndef ASSIGNMENT_SORTINGALGORITHMS_SORTINGALGORITHMS_H
+
+void swap(int *x, int *y);
+void printArray(int arr[], int size);
+void bubbleSort(int arr[], int n);
+void merge(int arr[], int p1, int m, int p2);
+void mergeSort(int arr[], int p1, int p2);
+void countingSort(int arr[], int n);
+
+int count1 = 0, nrP1 = 0, nrP2 = 0;
+
+#define ASSIGNMENT_SORTINGALGORITHMS_SORTINGALGORITHMS_H
+
+#endif //ASSIGNMENT_SORTINGALGORITHMS_SORTINGALGORITHMS_H
