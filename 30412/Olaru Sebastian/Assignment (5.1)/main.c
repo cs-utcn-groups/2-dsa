b6c5377ef0341325ebc39dbfcca43ac24973e173
Sebastian Olaru <sebastianolaru3008@gmail.com>
2020-05-27 16:31:47 +0300

    Assignment 5.1 commit

--- 30412/Olaru Sebastian/Assignment (5.1)/main.c
+++ 30412/Olaru Sebastian/Assignment (5.1)/main.c
@@ -1,8 +1,24 @@
-#include <stdio.h>
-
-
+#include "algorithms.h"
 
 int main() {
+    FILE *fin;
+    fin = fopen("input.dat", "r");
+
+    int noOfVertices;
+    int **adjMatrix = inputAdjMatrix(fin, &noOfVertices);
+    List *adjList = getListFromMatrix(adjMatrix, noOfVertices);
+
+    printf("\n\nThe adjacency list from given adjacency matrix:\n");
+    printAdjList(adjList, noOfVertices);
+
+    printf("\n\nThe given matrix from the computed adjacency list:\n");
+    adjMatrix = getMatrixFromList(adjList, noOfVertices);
+    printAdjMatrix(adjMatrix, noOfVertices);
+
+    printf("\n\nBFS on the resulted adjacency list:\n");
+    BFS(1, adjList, noOfVertices);
+    recursiveDFS(1, adjList, noOfVertices);
+
 
     return 0;
 }
